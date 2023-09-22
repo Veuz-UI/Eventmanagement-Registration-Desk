96eea771390b75f5e453af17ec67006dd51bbf31
anakha <anakha@veuz.in>
2023-09-22 18:52:58 +0530

    changes

--- agenda.php
+++ agenda.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="images/fav.webp" rel="icon" />
+    <link href="images/favicon.svg" rel="icon" />
     <title>Veuz</title>
     <link rel="stylesheet" href="css/icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
@@ -11,7 +11,7 @@
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link href="images/fav.webp" rel="icon" />
+
 </head>
 
 <body style="background-color: #f8f8f8;">
