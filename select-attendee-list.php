b0085493bc073271997553c14e0aea2bf1eac366
anakha <anakha@veuz.in>
2023-09-22 17:46:49 +0530

    changes

--- select-attendee-list.php
+++ select-attendee-list.php
@@ -458,7 +458,7 @@
     <script src="https://kit.fontawesome.com/49ccdaea81.js " crossorigin="anonymous "></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js"></script>
+    <script type="text/javascript" src="js/mdb.min.js"></script>
  <script>
         AOS.init({
             disable: 'mobile'
